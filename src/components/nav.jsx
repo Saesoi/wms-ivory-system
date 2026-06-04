fea5e4f07ecf4622e525724c43cd5f9b804c346f
Saesoi <eyoycabali90@gmail.com>
2026-06-04 10:52:07 +0800

    just a draft of css reset

---
+++ src/components/nav.jsx
@@ -0,0 +1,20 @@
+import './nav.css'
+
+export default function Nav() {
+  return(
+    <>  
+      <nav className="nav">
+        <div className='nav-left'>
+          <img className='logo' src="/src/assets/ivory-logo.jpg" alt="WMS Ivory Logo" />
+          <div>
+            <h3>WMS Ivory</h3>
+            <p>Lounge & Bar</p>
+          </div>
+        </div>
+        <div className='nav-right'>
+          <h1>he</h1>
+        </div>
+      </nav>
+    </>
+  )
+}
